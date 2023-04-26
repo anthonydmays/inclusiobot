f0f647844a04fea1062e05d1cf6cfb57e4947cbf
Anthony D. Mays <anthony@morganlatimer.com>
2023-04-25 22:39:01 -0700

    Removes svelte config and formats.

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,34 +1,34 @@
 module.exports = {
-	root: true,
-	parser: '@typescript-eslint/parser',
-	extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended'],
-	plugins: ['svelte3', '@typescript-eslint'],
-	ignorePatterns: ['*.cjs',"dist/**/*"],
-	overrides: [{ files: ['*.svelte'], processor: 'svelte3/svelte3' }],
-	settings: {
-		'svelte3/typescript': () => require('typescript')
-	},
-	parserOptions: {
-		sourceType: 'module',
-		ecmaVersion: 2020
-	},
-	env: {
-		browser: true,
-		es2021: true,
-		node: true
-	},
-	rules: {
-	  '@typescript-eslint/ban-types': [
-		'error',
-		{
-		  'extendDefaults': true,
-		  'types': {
-			'{}': false
-		  }
-		}
-	  ]
-	},
-	globals: {
-	  '__COMMIT_HASH__': true
-	}
+  root: true,
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  plugins: ['@typescript-eslint'],
+  ignorePatterns: ['*.cjs', 'dist/**/*'],
+  parserOptions: {
+    sourceType: 'module',
+    ecmaVersion: 2020,
+  },
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  rules: {
+    '@typescript-eslint/ban-types': [
+      'error',
+      {
+        extendDefaults: true,
+        types: {
+          '{}': false,
+        },
+      },
+    ],
+  },
+  globals: {
+    __COMMIT_HASH__: true,
+  },
 };
