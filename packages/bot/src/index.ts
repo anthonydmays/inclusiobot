3a88db11bc188e791440b152d3ff8e361dd5b03a
anthonydmays <anthony@morganlatimer.com>
2023-04-28 17:09:38 -0700

    Using $PORT for server by default.

--- packages/bot/src/index.ts
+++ packages/bot/src/index.ts
@@ -57,5 +57,7 @@ app.use('/v1', getApi(client));
 app.get('/', async (req, res) => {
   res.sendStatus(200);
 });
-app.listen(3000);
-console.log('Server started on port 3000');
+
+const port = process.env.PORT || 3000;
+app.listen(port);
+console.log(`Server started on port ${port}`);
